8f02d603372b00c6229395ce0934a6463e232bab
piotrsok2 <piotrsok2@users.noreply.github.com>
2021-09-14 15:57:45 +0200

    Fix examples to work with tensorflow 2.

--- tensorflow/examples/label_image/label_image.py
+++ tensorflow/examples/label_image/label_image.py
@@ -20,7 +20,8 @@ from __future__ import print_function
 import argparse
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def load_graph(model_file):
